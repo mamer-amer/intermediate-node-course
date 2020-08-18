dd8ecea73b4b162c83be073d4780e79944854186
mamer-amer <amer.rasool121@gmail.com>
2020-08-18 03:50:56 -0700

    changes done

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,5 @@
 const {MongoClient,ObjectID} = require('mongodb');
-var url = "mongodb://localhost:27017/demo";
+
 
 MongoClient.connect(url,{
     useUnifiedTopology: true,
--- server.js
+++ server.js
@@ -1,31 +1,43 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
-require('../intermediate-node-course/mongodb')
-const UserModel = require('../intermediate-node-course/models/User')
+// require('../intermediate-node-course/mongodb')
+const User = require('../intermediate-node-course/models/User')
 
+var url = "mongodb://localhost:27017/demo";
 const port=8000;
 const app= express();
+mongoose.connect(url,{useUnifiedTopology: true,
+  useNewUrlParser: true,useCreateIndex:true})
 
 app.use(bodyParser.json());
 
 
 // CREATE
-app.post('/users',(req,res)=>{
+app.post('/users',async (req,res)=>{
   console.log(req.body)
-  
-  
+
+  const user = new User(req.body);
+  const userObject = await user.save();
+  if(userObject) res.status(200).send(userObject)
+  res.send(500)
+
   
 })
 
 app.route('/users/:id')
 // READ
-.get((req,res)=>{
-  // User.findById()
+.get(async (req,res)=>{
+  const id = req.params.id;
+  const user = await User.findById(id)
+  res.send(user)
 })
 // UPDATE
-.put((req,res)=>{
-  // User.findByIdAndUpdate()
+.put(async (req,res)=>{
+  console.log(req)
+  const updatedUser = await User.findByIdAndUpdate({_id:req.params.id,})
+  res.send(updatedUser)
+
 })
 // DELETE
 .delete((req,res)=>{
